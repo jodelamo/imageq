72b92730064b89e1b5d95000a014309623abd05d
Joacim Löwgren <joacim.lowgren@gmail.com>
2016-03-31 20:05:47 +0200

    Add test for failing to load images

--- test/index.spec.js
+++ test/index.spec.js
@@ -6,6 +6,15 @@ describe('imageq', () => {
     expect(imageq).to.throw(Error);
   });
 
+  it('should reject with an error if an image fails to load', done => {
+    const images =['/1.png', '/does-not-exist.png'];
+
+    imageq(images).catch(err => {
+      expect(err).to.exist;
+      done();
+    });
+  });
+
   it('should return true for identical images', done => {
     const images = ['/2.png', '/3.png'];
 
