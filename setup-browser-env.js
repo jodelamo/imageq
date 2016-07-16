acb47b8234ba59fca0a5a1120d3606f81f41f829
Joacim Löwgren <joacim.lowgren@gmail.com>
2016-07-16 20:53:12 +0200

    Fix tests
    Studies show that making sure that promises actually resolves makes them
    all the more useful. Now using `canvas` and `jsdom` to properly mock
    image resource fetching.

--- setup-browser-env.js
+++ setup-browser-env.js
@@ -1,3 +1,7 @@
-global.document = require('jsdom').jsdom('<body></body>');
+global.document = require('jsdom').jsdom('<body></body>', {
+  features: {
+    FetchExternalResources: ['img']
+  }
+});
 global.window = document.defaultView;
 global.navigator = window.navigator;
--- test.js
+++ test.js
@@ -5,37 +5,37 @@ test('throw an error if its argument is not an array', t => {
   t.throws(imageq);
 });
 
-test('reject with an error if an image fails to load', t => {
+test('reject with an error if an image fails to load', async t => {
   const images = [
     './media/1.png',
     './media/does-not-exist.png'
   ];
 
-  imageq(images)
+  await imageq(images)
     .catch(err => {
       t.not(err, undefined);
     });
 });
 
-test('return true for identical images', t => {
+test('return true for identical images', async t => {
   const images = [
     './media/2.png',
     './media/3.png'
   ];
 
-  imageq(images)
+  await imageq(images)
     .then(identical => t.is(identical, true))
     .catch(err => t.fail(err));
 });
 
-test('return false for different images', t => {
+test('return false for different images', async t => {
   const images = [
     './media/1.png',
     './media/2.png',
     './media/3.png'
   ];
 
-  imageq(images)
+  await imageq(images)
     .then(identical => t.is(identical, false))
     .catch(err => t.fail(err));
 });
